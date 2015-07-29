a6a5656dd5bb4a96f277ff8f92f0d9d1569faae4
Brandon Rotkel <brotkel@gmail.com>
2015-07-29 01:24:40 -0700

    Added item_format function. Processing overview attachment with title, color, link, description, and icon.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ logger.level = process.env.LOG_LEVEL || 'debug';
 var search_parse_middleware = require('./middleware/search-parse');
 var wow_search_middleware = require('./middleware/wow-site-search');
 var wow_api_item_middleware = require('./middleware/wow-api-item');
+var item_format = require('./item-format');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -28,10 +29,11 @@ app.post('/item', function(req, resp) {
 	logger.debug('Item search occurred, link is ' + req.wowItem.webUrl);
 	var respChannel = "#" + req.body.channel_name;
 
-	var params = {
-		channel: respChannel,
-		text: "<" + req.wowItem.webUrl + "|" + req.wowItem.name + ">"
-	};
+	//var params = {
+	//	channel: respChannel,
+	//	text: "<" + req.wowItem.webUrl + "|" + req.wowItem.name + ">"
+	//};
+    var params = item_format(req.wowItem, respChannel);
 
 	slack.webhook(params, function(err, response) {
 		if (err) {
